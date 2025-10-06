c8850fd6c4af13ea2763babd563bdf53c355c2aa
AFCMS <afcm.contact@gmail.com>
2025-10-06 23:05:48 +0200

    feat: integrate Swagger for API documentation (fix #13)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Oktomusic")
+    .setLicense(
+      "AGPL-3.0-only",
+      "https://www.gnu.org/licenses/agpl-3.0.en.html",
+    )
+    .setDescription("Oktomusic API")
+    .setVersion("0.0.1")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
