81ad987b73c05a75cce9b71217f54dde50226d8e
AFCMS <afcm.contact@gmail.com>
2025-10-18 15:38:27 +0200

    fix(api): enable introspection for GraphQL module

--- apps/backend/src/api/api.module.ts
+++ apps/backend/src/api/api.module.ts
@@ -14,6 +14,7 @@ import { PrismaService } from "../db/prisma.service";
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       graphiql: true,
+      introspection: true,
       path: "/api/graphql",
       autoSchemaFile: path.join(__dirname, "schema.gql"),
       sortSchema: true,
