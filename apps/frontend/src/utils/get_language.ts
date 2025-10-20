f712a2a761f1e1adbf18bc30f83580cf8a9d8693
AFCMS <afcm.contact@gmail.com>
2025-10-20 21:26:47 +0200

    fix(frontend): stronger locale detection
    Fixes #57

--- apps/frontend/src/utils/get_language.ts
+++ apps/frontend/src/utils/get_language.ts
@@ -1,11 +1,17 @@
+import { detect, fromStorage, fromNavigator } from "@lingui/detect-locale";
+
 import supportedLocales, { type SupportedLocale } from "./supported_locales";
+
+const DEFAULT_FALLBACK = "en";
 
 export function isSupportedLocale(locale: string): locale is SupportedLocale {
   return (supportedLocales as readonly string[]).includes(locale);
 }
 
 export function getLanguage(): SupportedLocale {
-  const lang = navigator.language ?? "en";
+  const lang =
+    detect(fromStorage("lang"), fromNavigator(), DEFAULT_FALLBACK) ??
+    DEFAULT_FALLBACK;
   const sanitizedLang = lang.split("-")[0];
-  return isSupportedLocale(sanitizedLang) ? sanitizedLang : "en";
+  return isSupportedLocale(sanitizedLang) ? sanitizedLang : DEFAULT_FALLBACK;
 }
