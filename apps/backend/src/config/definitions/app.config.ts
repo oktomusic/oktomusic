07319083e2274b0a85bb5f41cc342477cb53d9ee
AFCMS <afcm.contact@gmail.com>
2025-11-26 15:20:52 +0100

    feat(backend): add a configuration value to specify intermediate folder
    For transcoding and other generated files

--- apps/backend/src/config/definitions/app.config.ts
+++ apps/backend/src/config/definitions/app.config.ts
@@ -1,9 +1,6 @@
-import fs from "node:fs";
-import path from "node:path";
-
 import { registerAs } from "@nestjs/config";
 import z from "zod";
-import { zBinaryPath } from "src/utils/zod";
+import { zBinaryPath, zFolderPath } from "../../utils/zod";
 
 const AppConfigSchema = z.object({
   /**
@@ -15,22 +12,8 @@ const AppConfigSchema = z.object({
     .enum(["development", "production", "test"])
     .default("development"),
   SESSION_SECRET: z.string().min(1),
-  LIBRARY_PATH: z.string().transform((arg, ctx) => {
-    const libPath = path.resolve(arg);
-    const exist = fs.existsSync(libPath);
-    const isDir = exist && fs.lstatSync(libPath).isDirectory();
-
-    if (!isDir) {
-      ctx.issues.push({
-        code: "custom",
-        message: "Library path must exist and be a directory",
-        input: libPath,
-      });
-      return z.NEVER;
-    }
-
-    return libPath;
-  }),
+  LIBRARY_PATH: zFolderPath("Library"),
+  INTERMEDIATE_PATH: zFolderPath("Intermediate", true),
   FFMPEG_PATH: zBinaryPath("FFMpeg", ["-version"]).optional(),
   FFPROBE_PATH: zBinaryPath("FFProbe", ["-version"]).optional(),
   METAFLAC_PATH: zBinaryPath("Metaflac", ["--version"]).optional(),
@@ -43,6 +26,7 @@ export interface AppConfig {
   isTest: boolean;
   sessionSecret: string;
   libraryPath: string;
+  intermediatePath: string;
   ffmpegPath: string | undefined;
   ffprobePath: string | undefined;
   metaflacPath: string | undefined;
@@ -58,6 +42,7 @@ export default registerAs("app", (): AppConfig => {
     isTest: parsed.NODE_ENV === "test",
     sessionSecret: parsed.SESSION_SECRET,
     libraryPath: parsed.LIBRARY_PATH,
+    intermediatePath: parsed.INTERMEDIATE_PATH,
     ffmpegPath: parsed.FFMPEG_PATH,
     ffprobePath: parsed.FFPROBE_PATH,
     metaflacPath: parsed.METAFLAC_PATH,
--- apps/backend/src/utils/zod.ts
+++ apps/backend/src/utils/zod.ts
@@ -59,4 +59,44 @@ function zBinaryPath(name: string, shouldRun?: string[]) {
   });
 }
 
-export { zBinaryPath };
+/**
+ * Zod transformer to validate a folder path
+ *
+ * @param name Display name of the directory for error logging purposes
+ * @param writable Whether to check if the directory is writable
+ * @returns
+ */
+function zFolderPath(name: string, writable?: boolean) {
+  return z.string().transform((arg, ctx) => {
+    const libPath = path.resolve(arg);
+    const exist = fs.existsSync(libPath);
+    const isDir = exist && fs.lstatSync(libPath).isDirectory();
+
+    if (!isDir) {
+      ctx.issues.push({
+        code: "custom",
+        message: `${name} path must exist and be a directory`,
+        input: libPath,
+      });
+      return z.NEVER;
+    }
+
+    if (writable) {
+      const isWritable =
+        fs.accessSync(libPath, fs.constants.W_OK) === undefined;
+
+      if (!isWritable) {
+        ctx.issues.push({
+          code: "custom",
+          message: `${name} path is not writable`,
+          input: libPath,
+        });
+        return z.NEVER;
+      }
+    }
+
+    return libPath;
+  });
+}
+
+export { zBinaryPath, zFolderPath };
