ded82f7939116f54b8f1294c8160c20d2e0d180e
AFCMS <afcm.contact@gmail.com>
2025-12-15 14:25:40 +0100

    feat(frontend): better login page with auto redirect support

--- apps/frontend/src/pages/Auth/Login.tsx
+++ apps/frontend/src/pages/Auth/Login.tsx
@@ -1,29 +1,36 @@
+import { useEffect } from "react";
 import { useAtomValue } from "jotai";
-import { Navigate } from "react-router";
+import { Navigate, useSearchParams } from "react-router";
 
 import { authSessionAtom } from "../../atoms/auth/atoms";
 
 export default function Login() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const authSession = useAtomValue(authSessionAtom);
+
+  useEffect(() => {
+    if (searchParams.get("auto_redirect") === "true") {
+      setSearchParams({});
+      window.location.href = "/api/auth/login";
+    }
+  }, [searchParams, setSearchParams]);
 
   if (authSession.status === "authenticated" && authSession.user) {
     return <Navigate to="/" replace />;
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        padding: "20px",
-      }}
-    >
-      <h1>Login</h1>
-      <p>Click the button below to login with your OIDC provider</p>
-      <a href="/api/auth/login">Login with OIDC</a>
+    <div className="flex h-full min-h-screen w-full sm:items-center sm:justify-center">
+      <div className="min-w-md rounded-md bg-sky-950 p-6">
+        <h1 className="mb-4 w-full text-center text-2xl">{"Login"}</h1>
+        <a
+          role="button"
+          className="align-center flex justify-center rounded-md bg-sky-900 p-4 text-center select-none hover:bg-sky-800"
+          href="/api/auth/login"
+        >
+          {"Login with OIDC"}
+        </a>
+      </div>
     </div>
   );
 }
