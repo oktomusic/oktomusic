6eca0b2a5667d875ed92b50dc43a50b542fa1060
AFCMS <afcm.contact@gmail.com>
2026-04-07 11:48:17 +0200

    feat(backend): boilerplate for XSPF playlist export
    https://xspf.org

--- apps/backend/src/api/playlist/playlist.controller.ts
+++ apps/backend/src/api/playlist/playlist.controller.ts
@@ -9,11 +9,39 @@ import { ApiOperation, ApiParam, ApiSecurity, ApiTags } from "@nestjs/swagger";
 import { AuthGuard } from "../../common/guards/auth.guard";
 
 import { PlaylistService } from "./playlist.service";
+import { ApiFile } from "../../common/decorators/api-file.decorator";
 
 @ApiTags("Media")
 @Controller("api/playlist")
 export class PlaylistController {
   constructor(private readonly playlistService: PlaylistService) {}
+
+  @Get(":cuid/xspf")
+  @UseGuards(AuthGuard)
+  @ApiSecurity("session")
+  @ApiOperation({
+    summary: "Fetch an playlist in XSPF format (refering to the libary root)",
+    description: "",
+  })
+  @ApiParam({
+    name: "cuid",
+    type: "string",
+    schema: { maxLength: 30 },
+    description: "Playlist CUID",
+    example: "tz4a98xxat96iws9zmbrgj3a",
+  })
+  @ApiFile({
+    contentType: "application/xspf+xml",
+    filenameExample: "Playlist_Name.xspf",
+    fileSizeExample: "123456",
+    successDescription:
+      "Playlist file returned successfully. Response includes Content-Length, Content-Type, and Content-Disposition headers.",
+  })
+  getXSPF() {
+    throw new NotImplementedException(
+      "XSPF playlist export not implemented yet",
+    );
+  }
 
   @Get(":cuid/m3u")
   @UseGuards(AuthGuard)
