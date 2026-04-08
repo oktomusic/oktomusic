afc547f85801ecca1c1070d1a8338a5b6fcd435f
AFCMS <afcm.contact@gmail.com>
2026-04-08 15:03:26 +0200

    feat(frontend): better right panel structure

--- apps/frontend/src/pages/PanelRight.tsx
+++ apps/frontend/src/pages/PanelRight.tsx
@@ -41,28 +41,52 @@ export function PanelRight() {
       className="flex flex-col overflow-hidden rounded bg-zinc-900"
       aria-label="Queue"
     >
-      <p className="p-2 text-sm font-semibold text-white/70">Queue</p>
-      <ol className="flex-1 overflow-auto px-2">
-        {queue[queueIndex] && (
-          <QueueTrack
-            track={queue[queueIndex]}
-            isCurrent={true}
-            isPlaying={isPlaying}
-            onClickPlay={() => handleTrackClick(queueIndex)}
-          />
-        )}
-        <li>
-          <hr className="my-1 border-zinc-700" />
+      <p className="p-4 text-sm font-semibold text-white/70">Queue</p>
+      <ol className="flex flex-1 flex-col gap-6 overflow-auto px-2">
+        <li className="flex flex-col">
+          <span className="p-2 text-sm font-semibold">Now Playing</span>
+          <ol className="flex flex-col">
+            {queue[queueIndex] && (
+              <QueueTrack
+                track={queue[queueIndex]}
+                isCurrent={true}
+                isPlaying={isPlaying}
+                onClickPlay={() => handleTrackClick(queueIndex)}
+              />
+            )}
+          </ol>
         </li>
-        {nextQueue.map((item, index) => (
-          <QueueTrack
-            key={index}
-            track={item}
-            isCurrent={false}
-            isPlaying={false}
-            onClickPlay={() => handleTrackClick(queueIndex + index + 1)}
-          />
-        ))}
+        <li className="flex flex-col">
+          <span className="p-2 text-sm font-semibold">Up Next</span>
+          <ol className="flex flex-col">
+            {nextQueue.map((item, index) => (
+              <QueueTrack
+                key={index}
+                track={item}
+                isCurrent={false}
+                isPlaying={false}
+                onClickPlay={() => handleTrackClick(queueIndex + index + 1)}
+              />
+            ))}
+          </ol>
+        </li>
+        {/*<li className="flex flex-col">
+          <div className="flex w-full flex-row justify-between p-2">
+            <span className="text-sm font-semibold">Queue from</span>
+            <button className="text-sm">Clear queue</button>
+          </div>
+          <ol className="flex flex-col">
+            {nextQueue.map((item, index) => (
+              <QueueTrack
+                key={index}
+                track={item}
+                isCurrent={false}
+                isPlaying={false}
+                onClickPlay={() => handleTrackClick(queueIndex + index + 1)}
+              />
+            ))}
+          </ol>
+        </li>*/}
       </ol>
     </aside>
   );
