772b0aed392781122f92770598a6de4f2fcd22bb
Klhmt <78567345+Klhmt@users.noreply.github.com>
2026-06-07 19:07:03 +0200

    fix SVG flag import + modify unify icon display on OktoListbox.tsx

--- apps/frontend/src/pages/SettingsClient/SettingsClient.tsx
+++ apps/frontend/src/pages/SettingsClient/SettingsClient.tsx
@@ -38,6 +38,8 @@ import { OktoInput } from "../../components/Base/OktoInput.tsx";
 import { OktoButton } from "../../components/Base/OktoButton.tsx";
 import { SupportedLocale } from "../../utils/supported_locales.ts";
 import { dynamicActivate } from "../../utils/i18n_loader.ts";
+import FrenchFlag from "../../assets/country_flags/french_flag.svg";
+import BritishFlag from "../../assets/country_flags/british_flag.svg";
 
 export function SettingsClient() {
   const [kioskMode, setKioskMode] = useAtom(settingClientKioskMode);
@@ -49,10 +51,13 @@ export function SettingsClient() {
     settingClientLyricsDisplayMode,
   );
   const [appLanguage, setAppLanguage] = useAtom(applicationLanguage);
-  const onAppLanguageChange = useCallback((appLanguage: SupportedLocale) => {
-    void dynamicActivate(appLanguage);
-    setAppLanguage(appLanguage);
-  }, [setAppLanguage]);
+  const onAppLanguageChange = useCallback(
+    (appLanguage: SupportedLocale) => {
+      void dynamicActivate(appLanguage);
+      setAppLanguage(appLanguage);
+    },
+    [setAppLanguage],
+  );
 
   const [lyricsTranslationEnabled, setLyricsTranslationEnabled] = useAtom(
     settingClientLyricsTranslationEnabled,
@@ -91,14 +96,13 @@ export function SettingsClient() {
       { value: "static", label: t`Static` },
     ];
 
-  // const languageDisplayLabels: Record<SupportedLocale, string> = {
-  //   fr: 'Français',
-  //   en: 'English',
-  // } as const;
-
   const languageDisplayLabels: OktoListboxItem<SupportedLocale>[] = [
-    { value: "en", label: 'English' },
-    {value: "fr", label: 'Français'}
+    { value: "en", label: "English", icon: BritishFlag },
+    {
+      value: "fr",
+      label: "Français",
+      icon: FrenchFlag,
+    },
   ];
 
   const handleCrossfadeChange = (value: number) => {
