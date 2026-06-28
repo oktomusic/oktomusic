dde82e7987f0211d8d2c586bf9a2ce844ff49194
AFCMS <afcm.contact@gmail.com>
2026-06-28 17:37:44 +0200

    feat(frontend): very basic home page

--- apps/frontend/src/pages/Center/Home.tsx
+++ apps/frontend/src/pages/Center/Home.tsx
@@ -1,5 +1,6 @@
 import { useAtomValue } from "jotai";
 import { Link } from "react-router";
+import { LuSearch, LuSettings } from "react-icons/lu";
 
 import { authSessionAtom } from "../../atoms/auth/atoms";
 
@@ -11,19 +12,28 @@ export function Home() {
   }
 
   return (
-    <>
-      <div className="card">
-        <div className="m-4 flex flex-col gap-2">
-          <div>
-            <p className="text-green-600">
-              ✓ Logged in as {authSession.user.username}
-            </p>
-          </div>
-          <div>
-            <Link to="/appinfo">App Info</Link>
-          </div>
-        </div>
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="m-4 flex flex-col justify-center gap-2">
+        <h2 className="mb-8 text-center text-3xl font-bold">
+          {document.title}
+        </h2>
+
+        <Link
+          className="flex w-72 cursor-pointer flex-row items-center gap-2 rounded-full bg-zinc-800 px-3 py-2 text-sm hover:bg-zinc-700 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-white/25"
+          to="/search"
+        >
+          <LuSearch className="mr-2 inline-block h-6 w-6" />
+          <span>Search</span>
+        </Link>
+
+        <Link
+          className="flex w-72 cursor-pointer flex-row items-center gap-2 rounded-full bg-zinc-800 px-3 py-2 text-sm hover:bg-zinc-700 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-white/25"
+          to="/settings/client"
+        >
+          <LuSettings className="mr-2 inline-block h-6 w-6" />
+          <span>Settings</span>
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
