a118ac40a7098813df810fd63b2d49f1974585c9
AFCMS <afcm.contact@gmail.com>
2026-08-25 17:21:24 +0200

    feat(frontend): switch to OXC based React Compiler

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { lingui, linguiTransformerBabelPreset } from "@lingui/vite-plugin";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import sri from "vite-plugin-sri-gen";
@@ -49,13 +49,9 @@ const viteConfigPWA: Partial<VitePWAOptions> = {
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({ compiler: true }),
     babel({
-      presets: [
-        reactCompilerPreset(),
-        "jotai-babel/preset",
-        linguiTransformerBabelPreset(),
-      ],
+      presets: ["jotai-babel/preset", linguiTransformerBabelPreset()],
     }),
     tailwindcss(),
     lingui(),
